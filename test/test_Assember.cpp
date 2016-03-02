997ad2b2d6487d280f38966dd96404a8fdff04e5
MForever78 <shunjian1128@gmail.com>
2016-03-02 21:02:56 +0800

    Add shift type assembly test

--- test/test_Assember.cpp
+++ test/test_Assember.cpp
@@ -18,9 +18,6 @@ TEST(AssemblerTest, RTypeInstructionTest) {
         "nor    $t0, $t1, $t2",
         "slt    $t0, $t1, $t2",
         "sltu   $t0, $t1, $t2",
-        //"sll    $t0, $t1, 15",
-        //"srl    $t0, $t1, 15",
-        //"sra    $t0, $t1, 15",
         "sllv   $t0, $t1, $t2",
         "srlv   $t0, $t1, $t2",
         "srav   $t0, $t1, $t2"
@@ -28,23 +25,40 @@ TEST(AssemblerTest, RTypeInstructionTest) {
     };
 
     std::uint32_t assemblyLiteral[] = {
-        0b00000001000010010101000000100000,     // add
-        0b00000001000010010101000000100001,     // addu
-        0b00000001000010010101000000100010,     // sub
-        0b00000001000010010101000000100011,     // subu
-        0b00000001000010010101000000100100,     // and
-        0b00000001000010010101000000100101,     // or
-        0b00000001000010010101000000100110,     // xor
-        0b00000001000010010101000000100111,     // nor
-        0b00000001000010010101000000101010,     // slt
-        0b00000001000010010101000000101011,     // sltu
-        //0b00000001000010010101001111000000,     // sll
-        //0b00000001000010010101001111000010,     // srl
-        //0b00000001000010010101001111000011,     // sra
-        0b00000001000010010101000000000100,     // sllv
-        0b00000001000010010101000000000110,     // srlv
-        0b00000001000010010101000000000111     // srav
+        0b00000001001010100100000000100000,     // add
+        0b00000001001010100100000000100001,     // addu
+        0b00000001001010100100000000100010,     // sub
+        0b00000001001010100100000000100011,     // subu
+        0b00000001001010100100000000100100,     // and
+        0b00000001001010100100000000100101,     // or
+        0b00000001001010100100000000100110,     // xor
+        0b00000001001010100100000000100111,     // nor
+        0b00000001001010100100000000101010,     // slt
+        0b00000001001010100100000000101011,     // sltu
+        0b00000001001010100100000000000100,     // sllv
+        0b00000001001010100100000000000110,     // srlv
+        0b00000001001010100100000000000111      // srav
         //0b00000001000000000000000000001000      // jr
+    };
+
+    Assembler assembler(std::vector<std::string> (instLiteral, instLiteral + sizeof(instLiteral) / sizeof(std::string)));
+    std::vector<Assembly> instAssembled = assembler.getInstAssembled();
+    std::vector<Assembly> assembly (assemblyLiteral, assemblyLiteral + sizeof(assemblyLiteral) / sizeof(std::uint32_t));
+
+    EXPECT_EQ(instAssembled, assembly);
+}
+
+TEST(AssemblerTest, ShiftTypeTest) {
+    std::string instLiteral[] = {
+        "sll    $t0, $t1, 15",
+        "srl    $t0, $t1, 15",
+        "sra    $t0, $t1, 15"
+    };
+
+    std::uint32_t assemblyLiteral[] = {
+        0b00000000000010000100101111000000,     // sll
+        0b00000000000010000100101111000010,     // srl
+        0b00000000000010000100101111000011      // sra
     };
 
     Assembler assembler(std::vector<std::string> (instLiteral, instLiteral + sizeof(instLiteral) / sizeof(std::string)));
