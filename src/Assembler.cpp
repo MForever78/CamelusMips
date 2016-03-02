06aff8fdd7c68533ac69d5905e225a0263e08a35
MForever78 <shunjian1128@gmail.com>
2016-03-02 22:24:28 +0800

    Reconstruct ctor

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -10,6 +10,33 @@
 
 using namespace std;
 
+const regex Assembler::rTypeExp ("(addu|add|subu|sub|and|or|xor|nor|sltu|slt|sllv|srlv|srav|jr|sll|srl|sra)\\b");
+const regex Assembler::iTypeExp ("(addiu|addi|andi|ori|xori|lui|lw|sw|beq|bne|sltiu|slti)\\b");
+const regex Assembler::jTypeExp ("(jal|j)\\b");
+
+const regex Assembler::operandExp ("\\$(\\w+)");
+const regex Assembler::operandsExp ("\\$(\\w+),\\s*\\$(\\w+),\\s*\\$(\\w+)");
+const regex Assembler::operandsWithImmExp ("\\$(\\w+),\\s*\\$(\\w+),\\s*(\\d+)");
+const map<string, int> Assembler::funcMap = {
+    {"addu",    0b100001},
+    {"add",     0b100000},
+    {"sub",     0b100010},
+    {"subu",    0b100011},
+    {"and",     0b100100},
+    {"or",      0b100101},
+    {"xor",     0b100110},
+    {"nor",     0b100111},
+    {"slt",     0b101010},
+    {"sltu",    0b101011},
+    {"sll",     0b000000},
+    {"srl",     0b000010},
+    {"sra",     0b000011},
+    {"sllv",    0b000100},
+    {"srlv",    0b000110},
+    {"srav",    0b000111},
+    {"jr",      0b001000}
+};
+
 Assembler::Assembler() {
 
 }
@@ -17,103 +44,83 @@ Assembler::Assembler() {
 Assembler::Assembler(const vector<string> &instOriginal) {
     this->instOriginal = instOriginal;
 
-    const static regex rTypeExp ("(addu|add|subu|sub|and|or|xor|nor|sltu|slt|sllv|srlv|srav|jr|sll|srl|sra)\\b");
-    const static regex iTypeExp ("(addiu|addi|andi|ori|xori|lui|lw|sw|beq|bne|sltiu|slti)\\b");
-    const static regex jTypeExp ("(jal|j)\\b");
-
-    const static regex operandExp ("\\$(\\w+)");
-    const static regex operandsExp ("\\$(\\w+),\\s*\\$(\\w+),\\s*\\$(\\w+)");
-    const static regex operandsWithImmExp ("\\$(\\w+),\\s*\\$(\\w+),\\s*(\\d+)");
-    const static map<string, int> funcMap = {
-        {"addu",    0b100001},
-        {"add",     0b100000},
-        {"sub",     0b100010},
-        {"subu",    0b100011},
-        {"and",     0b100100},
-        {"or",      0b100101},
-        {"xor",     0b100110},
-        {"nor",     0b100111},
-        {"slt",     0b101010},
-        {"sltu",    0b101011},
-        {"sll",     0b000000},
-        {"srl",     0b000010},
-        {"sra",     0b000011},
-        {"sllv",    0b000100},
-        {"srlv",    0b000110},
-        {"srav",    0b000111},
-        {"jr",      0b001000}
-    };
 
     for (auto inst: this->instOriginal) {
-        uint32_t opcode, rs, rt, rd, shamt, func;
         smatch match;
+        Assembly assembled;
 
-        if (regex_search(inst, match, rTypeExp)) {
+        if (regex_search(inst, match, Assembler::rTypeExp)) {
             // R-type
-
-            opcode = 0b000000;
-            string funcName = match[0];
-
-            string rsName, rtName, rdName, immName;
-            smatch operands, operandsWithImm, operand;
-
-            if (regex_search(inst, operands, operandsExp)) {
-                // Regular R-type
-                // i.e. Assembly with rs, rt, rd
-
-                rsName = operands[2];
-                rtName = operands[3];
-                rdName = operands[1];
-                rs = getOperand(rsName);
-                rt = getOperand(rtName);
-                rd = getOperand(rdName);
-                shamt = 0b00000;
-                func = funcMap.at(funcName);
-            } else if (regex_search(inst, operandsWithImm, operandsWithImmExp)) {
-                // Shift R-type
-                // i.e. Assembly with rt, rd, shamt
-
-                rdName = operandsWithImm[2];
-                rtName = operandsWithImm[1];
-                immName = operandsWithImm[3];
-                rs = 0b00000;
-                rd = getOperand(rdName);
-                rt = getOperand(rtName);
-                shamt = stoul(immName);
-                func = funcMap.at(funcName);
-            } else if (regex_search(inst, operand, operandExp)) {
-                // JR
-
-                rsName = operand[1];
-                rs = getOperand(rsName);
-                rd = 0b00000;
-                rt = 0b00000;
-                shamt = 0b00000;
-                func = funcMap.at(funcName);
-            } else {
-                // Syntax Error
-
-                cout << "Syntax error with statement:" << endl;
-                cout << inst << endl;
-            }
+            assembled = getRTypeAssembly(inst, match);
         }
 
-        uint32_t instruction = opcode;
-        instruction = (instruction << 5) | rs;
-        instruction = (instruction << 5) | rt;
-        instruction = (instruction << 5) | rd;
-        instruction = (instruction << 5) | shamt;
-        instruction = (instruction << 6) | func;
-
-        instAssembled.push_back(Assembly(instruction));
+        instAssembled.push_back(assembled);
     }
+}
+
+Assembly Assembler::getRTypeAssembly(const string &inst, const smatch &match) {
+    uint32_t opcode, rs, rt, rd, shamt, func;
+    opcode = 0b000000;
+    string funcName = match[0];
+
+    string rsName, rtName, rdName, immName;
+    smatch operands, operandsWithImm, operand;
+
+    if (regex_search(inst, operands, operandsExp)) {
+        // Regular R-type
+        // i.e. Assembly with rs, rt, rd
+
+        rsName = operands[2];
+        rtName = operands[3];
+        rdName = operands[1];
+        rs = getOperand(rsName);
+        rt = getOperand(rtName);
+        rd = getOperand(rdName);
+        shamt = 0b00000;
+        func = funcMap.at(funcName);
+    } else if (regex_search(inst, operandsWithImm, operandsWithImmExp)) {
+        // Shift R-type
+        // i.e. Assembly with rt, rd, shamt
+
+        rdName = operandsWithImm[2];
+        rtName = operandsWithImm[1];
+        immName = operandsWithImm[3];
+        rs = 0b00000;
+        rd = getOperand(rdName);
+        rt = getOperand(rtName);
+        shamt = stoul(immName);
+        func = funcMap.at(funcName);
+    } else if (regex_search(inst, operand, operandExp)) {
+        // JR
+
+        rsName = operand[1];
+        rs = getOperand(rsName);
+        rd = 0b00000;
+        rt = 0b00000;
+        shamt = 0b00000;
+        func = funcMap.at(funcName);
+    } else {
+        // Syntax Error
+
+        cout << "Syntax error with R-type statement:" << endl;
+        cout << inst << endl;
+    }
+
+    uint32_t instruction = opcode;
+    instruction = (instruction << 5) | rs;
+    instruction = (instruction << 5) | rt;
+    instruction = (instruction << 5) | rd;
+    instruction = (instruction << 5) | shamt;
+    instruction = (instruction << 6) | func;
+
+    return Assembly(instruction);
 }
 
 vector<Assembly> Assembler::getInstAssembled() {
     return instAssembled;
 }
 
-uint32_t Assembler::getOperand(string &operandName) {
+uint32_t Assembler::getOperand(const string &operandName) {
     const static string regNames[] = {
         "zero", "at",
         "v0", "v1", "a0", "a1", "a2", "a3",
--- src/Assembler.hpp
+++ src/Assembler.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cstdint>
 #include <iostream>
+#include <regex>
+#include <map>
 
 #include "Assembly.hpp"
 
@@ -18,10 +20,20 @@ public:
     friend std::ostream &operator<<(std::ostream &os, const Assembler &assembler);
 
 private:
+    const static std::regex rTypeExp;
+    const static std::regex iTypeExp;
+    const static std::regex jTypeExp;
+
+    const static std::regex operandExp;
+    const static std::regex operandsExp;
+    const static std::regex operandsWithImmExp;
+    const static std::map<std::string, int> funcMap;
+
     std::vector<std::string> instOriginal;
     std::vector<Assembly> instAssembled;
 
-    std::uint32_t getOperand(std::string &operandName);
+    std::uint32_t getOperand(const std::string &operandName);
+    Assembly getRTypeAssembly(const std::string &inst, const std::smatch &match);
 };
 
 #endif
