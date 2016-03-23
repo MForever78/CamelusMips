f2fe7d0ca389c8981f6fbd30ce16a5d4145692f2
MForever78 <shunjian1128@gmail.com>
2016-03-23 16:02:31 +0800

    chore: clean main function

--- src/main.cxx
+++ src/main.cxx
@@ -1,25 +1,8 @@
 #include <iostream>
-#include <vector>
-#include "Assembler.hpp"
-#include "Processor.hpp"
+#include "System.hpp"
 
 using namespace std;
 
 int main() {
-    const string instLiteral[] = {
-        "start:",
-        "       add    $t0, $zero, $zero",
-        "       addi   $t0, $t0, 15",
-        "loop:  add    $t1, $t0, $zero",
-        "       addi   $t0, $t0, -1",
-        "       bne    $t0, $zero, loop",
-        "exit:  j      start"
-    };
-
-    vector<string> inst(instLiteral, instLiteral + sizeof(instLiteral) / sizeof(string));
-    Assembler assembler(inst);
-
-    vector<Assembly> instAssembled = assembler.getInstAssembled();
-    Processor cpu(instAssembled);
-    cpu.run();
+    System soc;
 }
