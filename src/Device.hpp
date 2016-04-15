5a8e6e8e10d8b22023d15ea3b36e1f15ab4aa992
MForever78 <shunjian1128@gmail.com>
2016-04-15 22:07:04 +0800

    Add current operating address to Device class

--- src/Device.hpp
+++ src/Device.hpp
@@ -2,12 +2,18 @@
 #define DEVICE_HPP
 
 #include <cstdint>
+#include <iostream>
 
 class Device {
 public:
     virtual void set(const std::uint32_t data) = 0;
-    virtual std::uint32_t get(const std::uint32_t addr) const = 0;
-    virtual void setAddress(const std::uint32_t addr) = 0;
+    virtual std::uint32_t get() const = 0;
+
+    inline void setAddress(const std::uint32_t addr) {
+        operatingAddress = addr;
+    }
+protected:
+    std::uint32_t operatingAddress;
 };
 
 #endif
