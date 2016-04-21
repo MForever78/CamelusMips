1874dc54bd7b21226a3860df994dfa688cedc579
MForever78 <shunjian1128@gmail.com>
2016-04-21 10:13:05 +0800

    Add timer device

--- src/Timer.hpp
+++ src/Timer.hpp
@@ -2,13 +2,37 @@
 // Created by MForever78 on 16/4/19.
 //
 
-#ifndef CAMELUSMIPS_TIMER_HPP
-#define CAMELUSMIPS_TIMER_HPP
+#ifndef TIMER_HPP
+#define TIMER_HPP
 
+#include "Device.hpp"
 
-class Timer {
+class Timer: Device {
+public:
+    inline void set(const std::uint32_t data) override {
+        counting = true;
+        reverseCount = data;
+    }
+    inline std::uint32_t get() const override {
+        return clock;
+    };
+    void tick() {
+        clock++;
+        timeSlice++;
+        if (counting) reverseCount--;
 
+        if (reverseCount == 0) {
+            counting = false;
+            interrupting = true;
+        }
+    }
+
+private:
+    std::uint32_t clock = 0;
+    bool counting = false;
+    std::uint32_t reverseCount = 0;
+    std::uint32_t timeSlice = 0;
 };
 
 
-#endif //CAMELUSMIPS_TIMER_HPP
+#endif //TIMER_HPP
